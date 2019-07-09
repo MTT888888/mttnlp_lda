d0ace1917665aaa46f38cdf475790eae34696ebc
matongtong <1143298661@qq.com>
2019-07-09 08:52:28 +0800

    Replace source datas and output

--- LDA_model/Topics.py
+++ LDA_model/Topics.py
@@ -9,7 +9,7 @@ tr4w = TextRank4Keyword()
 tr4w.analyze(text=text, lower=True, window=2)   # py2中text必须是utf8编码的str或者unicode对象，py3中必须是utf8编码的bytes或者str对象
 
 print('关键词：')
-for item in tr4w.get_keywords(20, word_min_len=1):
+for item in tr4w.get_keywords(10, word_min_len=1):
     print(item.word, item.weight)
 
 print()
